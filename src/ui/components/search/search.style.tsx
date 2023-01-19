283d45bde131f040242440d35d836b5cfe3795ef
JPSS14 <jp_scarabelli@hotmail.com>
2023-01-19 15:02:01 -0300

    Add responsive style in Search component

--- src/ui/components/search/search.style.tsx
+++ src/ui/components/search/search.style.tsx
@@ -1,18 +1,30 @@
 import { Theme, SxProps } from '@mui/material';
 
-export const searchStyle: SxProps<Theme> = () => {
+export const searchStyle: SxProps<Theme> = (theme) => {
   return {
     display: 'flex',
     width: '100%',
     justifyContent: 'center',
 
-    'button':{
-      '&:last-child':{
+    'button': {
+      display: 'flex',
+      alignItems: 'center',
+      fontWeight: 700,
+      '& svg': {
+        fontSize: '1rem',
+        margin: `0px ${theme.spacing(0.3)}`
+      },
+      '&:last-child': {
         borderRadius: '0px 4px 4px 0px',
       },
-      '&:first-child':{
+      '&:first-child': {
         borderRadius: '4px 0px 0px 4px',
       }
+    },
+    '@media (max-width: 480px)': {
+      '& .button_title': {
+        display: 'none',
+      },
     }
   };
 };
--- src/ui/components/search/search.tsx
+++ src/ui/components/search/search.tsx
@@ -2,13 +2,21 @@ import { Box } from "@mui/material";
 import { Button } from "../button";
 import { Input } from "../input";
 import { searchStyle } from "./search.style";
+import StarIcon from '@mui/icons-material/Star';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const Search = () => {
   return (
     <Box sx={searchStyle}>
-      <Button color="primary" size='small'>Favoritos</Button>
+      <Button color="primary" size='small'>
+        <Box component='span' className='button_title'>Favoritos</Box>
+        <StarIcon />
+      </Button>
       <Input size="small" />
-      <Button color="primary" size='small'>Pesquisar</Button>
+      <Button color="primary" size='small'>
+        <SearchIcon />
+        <Box component='span' className='button_title'>Pesquisar</Box>
+      </Button>
     </Box>
   );
 };
