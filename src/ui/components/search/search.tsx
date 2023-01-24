8ef8164be5e21d0ebb4033052e554d1ff74b2b34
JPSS14 <jp_scarabelli@hotmail.com>
2023-01-24 17:56:38 -0300

    Add getSearch in Search component

--- src/ui/components/search/search.tsx
+++ src/ui/components/search/search.tsx
@@ -4,18 +4,32 @@ import { Input } from "../input";
 import { searchStyle } from "./search.style";
 import StarIcon from '@mui/icons-material/Star';
 import SearchIcon from '@mui/icons-material/Search';
+import { getSearch } from "../../../service/search.service";
+import { useState } from "react";
 
 export const Search = () => {
+  const [search, setSearch] = useState('');
+  const searchBook = () => {
+    if (search) {
+      getSearch(search);
+    }
+  };
+
+  const inputEnter = (input: string) => {
+    if(input === 'Enter'){
+      searchBook();
+    }
+  };
   return (
     <Box sx={searchStyle}>
       <Button color="primary">
         <Box component='span' className='button_title'>Favoritos</Box>
         <StarIcon />
       </Button>
-      <Input size="small" />
-      <Button color="primary">
+      <Input size="small" onChange={(e) => setSearch(e.currentTarget.value)} onKeyUp={(e) => inputEnter(e.key)}/>
+      <Button color="primary" onClick={() => searchBook()}>
         <SearchIcon />
-        <Box component='span' className='button_title'>Pesquisar</Box>
+        <Box component='span' className='button_title' >Pesquisar</Box>
       </Button>
     </Box>
   );
