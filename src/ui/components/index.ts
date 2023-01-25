221dc578b1e4751fa3a8ac74872c5da4771d639e
JPSS14 <jp_scarabelli@hotmail.com>
2023-01-25 16:54:06 -0300

    Fix app import webpack error

---
+++ src/ui/components/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './button';
+export { Header } from './header';
+export { Input } from './input';
+export { Layout } from './layout';
+export { Search } from './search';
+export { RBooksThemeProvider } from './theme'
