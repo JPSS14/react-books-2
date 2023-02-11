41d7e8d00e1a7dc1405dc9c2580baffb1d79e0f9
JPSS14 <jp_scarabelli@hotmail.com>
2023-02-10 21:13:04 -0300

    Add currencyConverter in helpers

--- src/helpers/utils/utils.test.ts
+++ src/helpers/utils/utils.test.ts
@@ -1,7 +1,15 @@
-import { dateConverter } from "./utils";
+import { currencyConverter, dateConverter } from "./utils";
 
 describe('dateConverter test', () => {
   test('a correct date test', () => {
     expect(dateConverter('2020-01-14')).toStrictEqual('14/01/2020');
+  });
+});
+
+describe('currencyConverter test', () => {
+  test('a correct currency test', () => {
+    const value = 39.9;
+    expect(currencyConverter(value))
+      .toBe(new Intl.NumberFormat('pt-Br', { style: 'currency', currency: 'BRL' }).format(value));
   });
 });
--- src/helpers/utils/utils.ts
+++ src/helpers/utils/utils.ts
@@ -2,3 +2,9 @@ export const dateConverter = (date: string) => {
   const newDate = new Date(date);
   return newDate.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
 };
+
+export const currencyConverter = (value: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency', currency: 'BRL'
+  }).format(value);
+};
