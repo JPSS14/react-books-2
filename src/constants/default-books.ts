82de9d39f89efd42226e3dfbc9c1805961944282
JPSS14 <jp_scarabelli@hotmail.com>
2023-02-16 23:57:33 -0300

    Add default-books constants

---
+++ src/constants/default-books.ts
@@ -0,0 +1,294 @@
+export const defaultBooks = [
+  {
+    "kind": "books#volume",
+    "id": "R7KuDwAAQBAJ",
+    "etag": "U4kPLzNoxNA",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/R7KuDwAAQBAJ",
+    "volumeInfo": {
+      "title": "O Senhor dos Anéis: A Sociedade do Anel",
+      "authors": [
+        "J.R.R. Tolkien"
+      ],
+      "publisher": "HARLEQUIN",
+      "publishedDate": "25/11/2019",
+      "description": "A Sociedade do Anel O volume inicial de O Senhor dos Anéis, lançado originalmente em julho de 1954, foi o primeiro grande épico de fantasia moderno, conquistando milhões de leitores e se tornando o padrão de referência para todas as outras obras do gênero até hoje. A imaginação prodigiosa de J.R.R. Tolkien e seu conhecimento profundo das antigas mitologias da Europa permitiram que ele criasse um universo tão complexo e convincente quanto o mundo real. A Sociedade do Anel começa no Condado, a região rural do oeste da Terra-média onde vivem os diminutos e pacatos hobbits. Bilbo Bolseiro, um dos raros aventureiros desse povo, cujas peripécias foram contadas em O Hobbit, resolve ir embora do Condado e deixa sua considerável herança nas mãos de seu jovem parente Frodo. O mais importante legado de Bilbo é o anel mágico que costumava usar para se tornar invisível. No entanto, o mago Gandalf, companheiro de aventuras do velho hobbit, revela a Frodo que o objeto é o Um Anel, a raiz do poder demoníaco de Sauron, o Senhor Sombrio, que deseja escravizar todos os povos da Terra-média. A única maneira de eliminar a ameaça de Sauron é destruir o Um Anel nas entranhas da própria montanha de fogo onde foi forjado. A revelação faz com que Frodo e seus companheiros hobbits Sam, Merry e Pippin deixem a segurança do Condado e iniciem uma perigosa jornada rumo ao leste. Ao lado de representantes dos outros Povos Livres que resistem ao Senhor Sombrio, eles formam a Sociedade do Anel. Alguém uma vez disse que o mundo dos leitores de língua inglesa se divide entre os que já leram O Senhor dos Anéis e os que um dia lerão o livro. Com esta nova tradução da obra, o fascínio dessa aventura atemporal ficará ainda mais evidente para os leitores brasileiros, tanto os que já conhecem a saga como os que estão prestes a descobrir seu encanto.",
+      "industryIdentifiers": [
+        {
+          "type": "ISBN_13",
+          "identifier": "9788595086333"
+        },
+        {
+          "type": "ISBN_10",
+          "identifier": "8595086338"
+        }
+      ],
+      "readingModes": {
+        "text": true,
+        "image": false
+      },
+      "pageCount": 584,
+      "printType": "BOOK",
+      "categories": [
+        "Fiction"
+      ],
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": true,
+      "contentVersion": "1.11.12.0.preview.2",
+      "panelizationSummary": {
+        "containsEpubBubbles": false,
+        "containsImageBubbles": false
+      },
+      "imageLinks": {
+        "smallThumbnail": "http://books.google.com/books/content?id=R7KuDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        "thumbnail": "http://books.google.com/books/content?id=R7KuDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      },
+      "language": "pt-BR",
+      "previewLink": "http://books.google.com.br/books?id=R7KuDwAAQBAJ&printsec=frontcover&dq=Senhor+dos+Aneis&hl=&cd=1&source=gbs_api",
+      "infoLink": "https://play.google.com/store/books/details?id=R7KuDwAAQBAJ&source=gbs_api",
+      "canonicalVolumeLink": "https://play.google.com/store/books/details?id=R7KuDwAAQBAJ"
+    },
+    "saleInfo": {
+      "country": "BR",
+      "saleability": "FOR_SALE",
+      "isEbook": true,
+      "listPrice": {
+        "amount": "R$ 49,90",
+        "currencyCode": "BRL"
+      },
+      "retailPrice": {
+        "amount": 47.4,
+        "currencyCode": "BRL"
+      },
+      "buyLink": "https://play.google.com/store/books/details?id=R7KuDwAAQBAJ&rdid=book-R7KuDwAAQBAJ&rdot=1&source=gbs_api",
+      "offers": [
+        {
+          "finskyOfferType": 1,
+          "listPrice": {
+            "amountInMicros": 49900000,
+            "currencyCode": "BRL"
+          },
+          "retailPrice": {
+            "amountInMicros": 47400000,
+            "currencyCode": "BRL"
+          },
+          "giftable": true
+        }
+      ]
+    },
+    "accessInfo": {
+      "country": "BR",
+      "viewability": "PARTIAL",
+      "embeddable": true,
+      "publicDomain": false,
+      "textToSpeechPermission": "ALLOWED",
+      "epub": {
+        "isAvailable": true,
+        "acsTokenLink": "http://books.google.com.br/books/download/O_Senhor_dos_An%C3%A9is_A_Sociedade_do_Anel-sample-epub.acsm?id=R7KuDwAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+      },
+      "pdf": {
+        "isAvailable": false
+      },
+      "webReaderLink": "http://play.google.com/books/reader?id=R7KuDwAAQBAJ&hl=&source=gbs_api",
+      "accessViewStatus": "SAMPLE",
+      "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+      "textSnippet": "A imaginação prodigiosa de J.R.R. Tolkien e seu conhecimento profundo das antigas mitologias da Europa permitiram que ele criasse um universo tão complexo e convincente quanto o mundo real."
+    }
+  },
+  {
+    "kind": "books#volume",
+    "id": "TnXrDAAAQBAJ",
+    "etag": "23xMQqDSiyk",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/TnXrDAAAQBAJ",
+    "volumeInfo": {
+      "title": "Warcraft: livro do filme oficial",
+      "authors": [
+        "Christie Golden"
+      ],
+      "publisher": "Galera",
+      "publishedDate": "26/08/2016",
+      "description": "A versão em livro do filme de ação e fantasia Warcraft. Faz tempo que Azeroth está em paz. Após expulsar os trolls, com a ajuda de Medivh, Guardião do reino, os humanos vivem em paz com os elfos e anões. Mas uma raça temerária de invasores, os guerreiros orcs, insuflados pelo feiticeiro Gul'dan e liderados pelo monstruoso Mão Negra, fogem de seu mundo agonizante em busca de caça e oportunidades. Gul'dan criou um portal capaz de transportar sua Horda até Azeroth. Morte e destruição ameaçam destruir a tudo e a todos. Então, de lados opostos, dois heróis surgem, em uma rota de colisão que decidirá o destino de sua família, seu povo e seu lar. Durotan, o líder honrado do clã Lobo do Gelo, quer apenas uma chance para seu filho recém-nascido. Lothar, o Leão de Azeroth, busca redenção. E assim começa uma espetacular saga sobre poder e sacrifício, na qual a guerra tem muitas facetas e todos lutam por algo.",
+      "industryIdentifiers": [
+        {
+          "type": "ISBN_13",
+          "identifier": "9788501108005"
+        },
+        {
+          "type": "ISBN_10",
+          "identifier": "8501108006"
+        }
+      ],
+      "readingModes": {
+        "text": true,
+        "image": true
+      },
+      "pageCount": 266,
+      "printType": "BOOK",
+      "categories": [
+        "Fiction"
+      ],
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": false,
+      "contentVersion": "1.5.5.0.preview.3",
+      "panelizationSummary": {
+        "containsEpubBubbles": false,
+        "containsImageBubbles": false
+      },
+      "imageLinks": {
+        "smallThumbnail": "http://books.google.com/books/content?id=TnXrDAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        "thumbnail": "http://books.google.com/books/content?id=TnXrDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      },
+      "language": "pt-BR",
+      "previewLink": "http://books.google.com.br/books?id=TnXrDAAAQBAJ&printsec=frontcover&dq=Warcraft&hl=&cd=2&source=gbs_api",
+      "infoLink": "https://play.google.com/store/books/details?id=TnXrDAAAQBAJ&source=gbs_api",
+      "canonicalVolumeLink": "https://play.google.com/store/books/details?id=TnXrDAAAQBAJ"
+    },
+    "saleInfo": {
+      "country": "BR",
+      "saleability": "FOR_SALE",
+      "isEbook": true,
+      "listPrice": {
+        "amount": "R$ 37,90",
+        "currencyCode": "BRL"
+      },
+      "retailPrice": {
+        "amount": 37.9,
+        "currencyCode": "BRL"
+      },
+      "buyLink": "https://play.google.com/store/books/details?id=TnXrDAAAQBAJ&rdid=book-TnXrDAAAQBAJ&rdot=1&source=gbs_api",
+      "offers": [
+        {
+          "finskyOfferType": 1,
+          "listPrice": {
+            "amountInMicros": 37900000,
+            "currencyCode": "BRL"
+          },
+          "retailPrice": {
+            "amountInMicros": 37900000,
+            "currencyCode": "BRL"
+          },
+          "giftable": true
+        }
+      ]
+    },
+    "accessInfo": {
+      "country": "BR",
+      "viewability": "PARTIAL",
+      "embeddable": true,
+      "publicDomain": false,
+      "textToSpeechPermission": "ALLOWED",
+      "epub": {
+        "isAvailable": true,
+        "acsTokenLink": "http://books.google.com.br/books/download/Warcraft_livro_do_filme_oficial-sample-epub.acsm?id=TnXrDAAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+      },
+      "pdf": {
+        "isAvailable": true,
+        "acsTokenLink": "http://books.google.com.br/books/download/Warcraft_livro_do_filme_oficial-sample-pdf.acsm?id=TnXrDAAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+      },
+      "webReaderLink": "http://play.google.com/books/reader?id=TnXrDAAAQBAJ&hl=&source=gbs_api",
+      "accessViewStatus": "SAMPLE",
+      "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+      "textSnippet": "A versão em livro do filme de ação e fantasia Warcraft."
+    }
+  },
+  {
+    "kind": "books#volume",
+    "id": "7S6OAwAAQBAJ",
+    "etag": "XDTVdeXi0MU",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/7S6OAwAAQBAJ",
+    "volumeInfo": {
+      "title": "Tarkin: Star Wars",
+      "authors": [
+        "James Luceno"
+      ],
+      "publisher": "Del Rey",
+      "publishedDate": "04/11/2014",
+      "description": "A long time ago in a galaxy far, far away. . . . Bestselling Star Wars veteran James Luceno gives Grand Moff Tarkin the Star Wars: Darth Plagueis treatment, bringing the legendary character from A New Hope to full, fascinating life. He’s the scion of an honorable and revered family. A dedicated soldier and distinguished legislator. Loyal proponent of the Republic and trusted ally of the Jedi Order. Groomed by the ruthless politician and Sith Lord who would be Emperor, Governor Wilhuff Tarkin rises through the Imperial ranks, enforcing his authority ever more mercilessly . . . and zealously pursuing his destiny as the architect of absolute dominion. Rule through the fear of force rather than force itself, he advises his Emperor. Under Tarkin’s guidance, an ultimate weapon of unparalleled destruction moves ever closer to becoming a terrifying reality. When the so-called Death Star is completed, Tarkin is confident that the galaxy’s lingering pockets of Separatist rebellion will be brought to heel—by intimidation . . . or annihilation. Until then, however, insurgency remains a genuine threat. Escalating guerrilla attacks by resistance forces and newfound evidence of a growing Separatist conspiracy are an immediate danger the Empire must meet with swift and brutal action. And to bring down a band of elusive freedom fighters, the Emperor turns to his most formidable agents: Darth Vader, the fearsome new Sith enforcer as remorseless as he is mysterious; and Tarkin—whose tactical cunning and cold-blooded efficiency will pave the way for the Empire’s supremacy . . . and its enemies’ extinction. Praise for Tarkin “Tarkin tells a compelling tale of mystery while revealing much about a character who has fueled debate among fans since 1977, as well as the oppressive regime he represents. [James] Luceno has proven once again that the villains of Star Wars are as much fun as the good guys.”—New York Daily News “Another home run in the new canon . . . This is the highest and best distillation of Tarkin stories, old and new, we’ve ever been given.”—Big Shiny Robot “A spectacular novel, with the intrigue, action, and profound characterization we have come to expect from the pen of Luceno . . . [Tarkin] provides a nuanced, multi-faceted anti-hero who is captivating, ominous, and calculating; in many ways, this is an origin tale of the Empire [and] a fascinating portrait of one of the more popular characters in the Original Trilogy. It may very well be one my favorite Star Wars novels.”—Coffee with Kenobi “A fascinating look at the pathos of one of the galaxy’s most criminally underused characters.”—TheForce.net",
+      "industryIdentifiers": [
+        {
+          "type": "ISBN_13",
+          "identifier": "9780553392890"
+        },
+        {
+          "type": "ISBN_10",
+          "identifier": "0553392891"
+        }
+      ],
+      "readingModes": {
+        "text": true,
+        "image": false
+      },
+      "pageCount": 288,
+      "printType": "BOOK",
+      "categories": [
+        "Fiction"
+      ],
+      "maturityRating": "NOT_MATURE",
+      "allowAnonLogging": true,
+      "contentVersion": "2.8.10.0.preview.2",
+      "panelizationSummary": {
+        "containsEpubBubbles": false,
+        "containsImageBubbles": false
+      },
+      "imageLinks": {
+        "smallThumbnail": "http://books.google.com/books/content?id=7S6OAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+        "thumbnail": "http://books.google.com/books/content?id=7S6OAwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+      },
+      "language": "en",
+      "previewLink": "http://books.google.com.br/books?id=7S6OAwAAQBAJ&pg=PA85&dq=Star+Wars+Tarkin&hl=&cd=6&source=gbs_api",
+      "infoLink": "https://play.google.com/store/books/details?id=7S6OAwAAQBAJ&source=gbs_api",
+      "canonicalVolumeLink": "https://play.google.com/store/books/details?id=7S6OAwAAQBAJ"
+    },
+    "saleInfo": {
+      "country": "BR",
+      "saleability": "FOR_SALE",
+      "isEbook": true,
+      "listPrice": {
+        "amount": "R$ 29,03",
+        "currencyCode": "BRL"
+      },
+      "retailPrice": {
+        "amount": 29.03,
+        "currencyCode": "BRL"
+      },
+      "buyLink": "https://play.google.com/store/books/details?id=7S6OAwAAQBAJ&rdid=book-7S6OAwAAQBAJ&rdot=1&source=gbs_api",
+      "offers": [
+        {
+          "finskyOfferType": 1,
+          "listPrice": {
+            "amountInMicros": 29030000,
+            "currencyCode": "BRL"
+          },
+          "retailPrice": {
+            "amountInMicros": 29030000,
+            "currencyCode": "BRL"
+          },
+          "giftable": true
+        }
+      ]
+    },
+    "accessInfo": {
+      "country": "BR",
+      "viewability": "PARTIAL",
+      "embeddable": true,
+      "publicDomain": false,
+      "textToSpeechPermission": "ALLOWED",
+      "epub": {
+        "isAvailable": true,
+        "acsTokenLink": "http://books.google.com.br/books/download/Tarkin_Star_Wars-sample-epub.acsm?id=7S6OAwAAQBAJ&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+      },
+      "pdf": {
+        "isAvailable": false
+      },
+      "webReaderLink": "http://play.google.com/books/reader?id=7S6OAwAAQBAJ&hl=&source=gbs_api",
+      "accessViewStatus": "SAMPLE",
+      "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+      "textSnippet": "“The Separatist war machines were shut down, Dangor went on. ... “As Governor Tarkin can attest, our navy was still chasing Separatist holdouts as late as a year ago. ... Rancit began, “back when he was merely \u003cb\u003eSTAR WARS\u003c/b\u003e: \u003cb\u003eTARKIN\u003c/b\u003e 85."
+    }
+  },
+];
