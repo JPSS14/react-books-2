c9df60e40d1b5854008353775774fabfd7351713
JPSS14 <jp_scarabelli@hotmail.com>
2023-03-18 20:07:57 -0300

    Add padding in Card img

--- src/ui/components/card/card.style.tsx
+++ src/ui/components/card/card.style.tsx
@@ -37,7 +37,7 @@ export const CardImgContainerStyle: SxProps<Theme> = (theme) => {
     justifyContent: 'center',
     width: '100%',
     height: '250px',
-    padding: theme.spacing(1),
+    padding: theme.spacing(2),
     '& img': {
       boxShadow: 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
     }
