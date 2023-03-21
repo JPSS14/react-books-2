031d20634580fbbc3887d3b2af89d5622066f8a5
JPSS14 <jp_scarabelli@hotmail.com>
2023-03-20 23:17:45 -0300

    Fix grid responsive bug

--- src/ui/components/card-list/card-list.style.tsx
+++ src/ui/components/card-list/card-list.style.tsx
@@ -2,12 +2,20 @@ import { SxProps, Theme } from '@mui/material';
 
 export const CardListContainerStyle:SxProps<Theme> = (theme) => {
   return {
-    width: '100%',
-    justifyContent: 'center',
-    padding: `${theme.spacing(2)} 0`,
+    maxWidth: '1264px',
+    padding: `${theme.spacing(6)} 0 ${theme.spacing(12)} 0`,
     '& li':{
       listStyle: 'none'
     },
     margin: 0,
+    '@media(max-width: 1262.4px)':{
+      maxWidth: '948px'
+    },
+    '@media(max-width: 948px)':{
+      maxWidth: '632px'
+    },
+    '@media(max-width: 632px)':{
+      maxWidth: '316px'
+    },
   };
 };
