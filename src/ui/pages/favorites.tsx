ced28b2b5a2c256297e57c9a04d6e234b88f4a7f
JPSS14 <jp_scarabelli@hotmail.com>
2023-10-11 10:58:09 -0300

    Add all pages in folders

--- src/ui/pages/favorites.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-
-export const Favorites = () => {
-  return (
-    <Box>
-      <h1>Favorites</h1>
-    </Box>
-  );
-};
---
+++ src/ui/pages/favorites/favorites.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import { Layout } from "ui/components";
+
+export const Favorites = () => {
+  return (
+    <Layout>
+      <Box>
+        <Typography component="h2">Meus Favoritos</Typography>
+      </Box>
+    </Layout>
+  );
+};
