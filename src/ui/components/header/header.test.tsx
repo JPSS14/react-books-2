aa8d9a487e5bca237606938ce6de7ab1f26faa50
JPSS14 <jp_scarabelli@hotmail.com>
2023-11-02 16:56:58 -0300

    Add test in Header

---
+++ src/ui/components/header/header.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { Header } from "./header";
+import { theme } from "ui/components/theme/theme-default";
+
+describe("Header component", () => {
+  test("renders the header with the correct text and link", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </ThemeProvider>
+    );
+
+    const link = screen.getByRole("link", { name: /React Books 2/ });
+
+    expect(screen.getByText(/React/)).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "/react-books-2");
+  });
+});
