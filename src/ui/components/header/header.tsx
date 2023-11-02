e0c4f0755bc1f307f011b44a91c48bcaf3e779ea
JPSS14 <jp_scarabelli@hotmail.com>
2023-11-02 01:16:51 -0300

    Refactor header component semantics

--- src/ui/components/header/header.tsx
+++ src/ui/components/header/header.tsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <Box sx={headerContainerStyle}>
-      <Typography component="h1">
-        <Link to="/react-books-2">
-          React <Box component="span">Books</Box> 2
-        </Link>
-      </Typography>
+    <Box sx={headerContainerStyle} component="header">
+      <Box component="nav">
+        <Typography component="h1">
+          <Link to="/react-books-2">
+            React <Box component="span">Books</Box> 2
+          </Link>
+        </Typography>
+      </Box>
     </Box>
   );
 };
