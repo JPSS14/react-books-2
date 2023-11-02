47ba5176e2c535ede86bc971e688eb39fe4a3a5f
JPSS14 <jp_scarabelli@hotmail.com>
2023-11-02 01:05:10 -0300

    Add SeactionHeder in Book page

--- src/ui/pages/book/book.tsx
+++ src/ui/pages/book/book.tsx
@@ -1,8 +1,10 @@
 import { Box } from "@mui/material";
+import { SectionHeader } from "ui/components";
 
 export const Book = () => {
   return (
     <Box>
+      <SectionHeader />
       <h1>Book </h1>
     </Box>
   );
