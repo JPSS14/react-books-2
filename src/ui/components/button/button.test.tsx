c713ce3b49eb1f53df426951846fa2d96e722b78
JPSS14 <jp_scarabelli@hotmail.com>
2023-11-07 21:56:59 -0300

    Add describe in Button test

--- src/ui/components/button/button.test.tsx
+++ src/ui/components/button/button.test.tsx
@@ -1,16 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { Button } from "./button";
 
-test("renders the button with the correct text", () => {
-  const mockProps = {
-    children: "Button",
-    onClick: jest.fn(),
-    disabled: true,
-  };
+describe("button test", () => {
+  test("renders the button with the correct text", () => {
+    const mockProps = {
+      children: "Button",
+      onClick: jest.fn(),
+      disabled: true,
+    };
 
-  render(<Button {...mockProps} />);
-  const renderButton = screen.getByText("Button");
+    render(<Button {...mockProps} />);
+    const renderButton = screen.getByText("Button");
 
-  expect(renderButton).toBeInTheDocument();
-  expect(renderButton).toHaveAttribute("disabled");
+    expect(renderButton).toBeInTheDocument();
+    expect(renderButton).toHaveAttribute("disabled");
+  });
 });
