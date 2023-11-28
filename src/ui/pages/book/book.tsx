fd2e1fb2f80fa0625deb9d4b2b3159e0d307b097
JPSS14 <jp_scarabelli@hotmail.com>
2023-11-27 21:16:09 -0300

    Add BookDetail in book page

--- src/ui/pages/book/book.tsx
+++ src/ui/pages/book/book.tsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
-import { SectionHeader } from "ui/components";
+import { useSearchContext } from "contexts/search-context/search-context";
+import { BookDetail, Layout, Loading, SectionHeader } from "ui/components";
+import { getByBookID } from "service/search.service";
+import { useParams } from "react-router-dom";
 
 export const Book = () => {
+  const { activeBook } = useSearchContext();
+  const [localActiveBook, setLocalActiveBook] = useState(activeBook);
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (!activeBook && id) {
+      setLoading(true);
+      getByBookID(id)
+        .then((item) => {
+          setLocalActiveBook(item);
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [id, activeBook]);
+
   return (
-    <Box component="main">
-      <SectionHeader title="Titulo do livro?" backToHome />
-      <h1>Book </h1>
-    </Box>
+    <Layout>
+      <Box component="main">
+        <SectionHeader title="Detalhes" backToHome />
+        {loading || !localActiveBook ? (
+          <Box
+            sx={{
+              width: "100%",
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Loading />
+          </Box>
+        ) : (
+          <>
+            <BookDetail item={localActiveBook} />
+          </>
+        )}
+      </Box>
+    </Layout>
   );
 };
