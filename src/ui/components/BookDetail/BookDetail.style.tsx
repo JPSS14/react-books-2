a5df7be120ac5774211547d4b313dc7374b98b82
JPSS14 <jp_scarabelli@hotmail.com>
2023-12-05 19:13:35 -0300

    Add book detail components in BookDetail

--- src/ui/components/BookDetail/BookDetail.style.tsx
+++ src/ui/components/BookDetail/BookDetail.style.tsx
@@ -10,55 +10,20 @@ export const bookDetailStyle: SxProps<Theme> = (theme) => {
     padding: theme.spacing(2),
     border: `3px solid ${rbooks.primaryColor}`,
     borderRadius: "10px",
-    ".info": {
-      fontWeight: 700,
-      color: "#fff",
-    },
-    ".book__container": {
-      display: "flex",
-      flexWrap: "wrap",
-      width: "200px",
-      "& .detail__image-container": {
-        display: "flex",
-        justifyContent: "center",
-        width: "100%",
-        height: "218px",
-        "& img": {
-          boxShadow:
-            "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
-        },
-      },
-    },
     ".detail__container": {
       width: "calc(100% - 216px)",
       marginLeft: theme.spacing(2),
-      ".detail__header": {
-        width: "100%",
-        "& h1": {
-          fontWeight: 700,
-          fontSize: "1.3rem",
-          color: "#fff",
-        },
+      height: "310.2px",
+      ".detail__content": {
+        height: "318.2px",
       },
       ".detail__description": {
         ".detail__description-content": {
-          height: "200px",
+          height: "154px",
           overflow: "hidden",
           overflowY: "scroll",
         },
       },
     },
-    ".detail__cta": {
-      width: "100%",
-      borderTop: `3px solid ${rbooks.primaryColor}`,
-    },
-  };
-};
-
-export const bookDetailFooterStyle: SxProps<Theme> = (theme) => {
-  const { rbooks } = theme.palette;
-  return {
-    width: "100%",
-    borderTop: `3px solid ${rbooks.primaryColor}`,
   };
 };
--- src/ui/components/BookDetail/BookDetail.tsx
+++ src/ui/components/BookDetail/BookDetail.tsx
@@ -1,7 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { BookItemResponseMapper } from "service/types";
-import { bookDetailFooterStyle, bookDetailStyle } from "./BookDetail.style";
-import { Button } from "../Button";
+import { bookDetailStyle } from "./BookDetail.style";
+import { BookDetailCTA } from "./BookDetailCTA";
+import { Hr } from "ui/components/Hr";
+import { BookDetailHeader } from "./BookDetailHeader";
+import { BookDetailContent } from "./BookDetailContent";
+import { BookDetailFooter } from "./BookDetailFooter";
 
 interface BookDetailProps {
   item: BookItemResponseMapper;
@@ -10,65 +14,39 @@ interface BookDetailProps {
 export const BookDetail = ({ item }: BookDetailProps) => {
   return (
     <Box sx={bookDetailStyle} component="article">
-      <Box className="book__container">
-        <Box className="detail__image-container">
-          {item.volumeInfo.imageLinks?.thumbnail ? (
-            <img
-              src={item.volumeInfo.imageLinks.thumbnail}
-              alt={item.volumeInfo.title}
-              title={item.volumeInfo.title}
-            />
-          ) : (
-            "Sem imagem"
-          )}
-        </Box>
-        <Box className="detail__cta">
-          <Typography component="p">
-            {item.saleInfo.listPrice?.amount}
-          </Typography>
-          <Box component="span">Average Rating</Box>
-          <Button variant="outlined">Ler Amostra</Button>
-          <Button>Comprar</Button>
-        </Box>
-      </Box>
+      <BookDetailCTA
+        image={item.volumeInfo.imageLinks?.thumbnail}
+        title={item.volumeInfo.title}
+        amount={item.saleInfo.listPrice?.amount}
+        rating={item.volumeInfo.averageRating}
+        ratingsCount={item.volumeInfo.ratingsCount}
+        buyLink={item.saleInfo.buyLink}
+        readLink={item.volumeInfo.previewLink}
+      />
       <Box className="detail__container">
-        <Box component="header" className="detail__header">
-          <Typography component="h1">{item.volumeInfo.title}</Typography>
-          <Typography>{item.searchInfo?.textSnippet}</Typography>
-          <Typography component="p">
-            <Box component="span" className="info">
-              Autor(es):
-            </Box>{" "}
-            {item.volumeInfo.authors}
-          </Typography>
-          <Typography component="p">
-            <Box component="span" className="info">
-              Editora:
-            </Box>{" "}
-            {item.volumeInfo.publisher}
-          </Typography>
-          <Typography component="p">
-            <Box component="span" className="info">
-              Data de Publicação:
-            </Box>{" "}
-            {item.volumeInfo.publishedDate}
-          </Typography>
-        </Box>
-        <Box className="detail__description">
-          <Typography component="p" className="info">
-            Descrição:
-          </Typography>
-          <Box className="detail__description-content">
-            <Typography component="p">{item.volumeInfo.description}</Typography>
+        <Box className="detail__content">
+          <Box className="detail__description">
+            <BookDetailHeader
+              title={item.volumeInfo.title}
+              textSnippet={item.searchInfo?.textSnippet}
+            />
+            <BookDetailContent
+              authors={item.volumeInfo.authors}
+              publisher={item.volumeInfo.publisher}
+              publishedDate={item.volumeInfo.publishedDate}
+              description={item.volumeInfo.description}
+            />
           </Box>
         </Box>
-      </Box>
-      <Box component="footer" sx={bookDetailFooterStyle}>
-        Metadata
-        <Box>
-          Número de páginas, Idioma, Editora??, Data de publicação, ISNB-10,
-          ISNB-13
-        </Box>
+        <Hr isMargin />
+        <BookDetailFooter
+          pageCount={item.volumeInfo.pageCount}
+          language={item.volumeInfo.language}
+          publisher={item.volumeInfo.publisher}
+          publishedDate={item.volumeInfo.publishedDate}
+          isnb13={item.volumeInfo.industryIdentifiers[0]}
+          isnb10={item.volumeInfo.industryIdentifiers[1]}
+        />
       </Box>
     </Box>
   );
