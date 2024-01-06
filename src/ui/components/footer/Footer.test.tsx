5f9677d17794e7d5611c419240b2a578e1e2d186
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-05 23:11:38 -0300

    Add test in Footer component

---
+++ src/ui/components/footer/Footer.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "@mui/material";
+import { MemoryRouter } from "react-router-dom";
+import { theme } from "ui/components/Theme/theme-default";
+import { Footer } from "./Footer";
+
+describe("Footer", () => {
+  test("a correct Footer render", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <MemoryRouter>
+          <Footer />
+        </MemoryRouter>
+      </ThemeProvider>
+    );
+
+    const footerRender = screen.getByText(/React/);
+    const link = screen.getByRole("link", { name: "React Books 2" });
+
+    expect(footerRender).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "/react-books-2");
+  });
+});
